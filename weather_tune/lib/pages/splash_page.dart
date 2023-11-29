2bf7d9e4c10140dfccaab0b22d5188981ba369b3
Adrià Pons Mensa <adria.pons.mensa@estudiantat.upc.edu>
2023-11-29 12:01:37 +0100

    splash_page.dart implemented and working
    Implemented the splash screen. It is the first screen it loads when running the app, and it jumps automatically to the login/register screen after 4 seconds right now. This value can be changed.

--- weather_tune/lib/pages/splash_page.dart
+++ weather_tune/lib/pages/splash_page.dart
@@ -1,15 +1,76 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:weather_tune/auth/auth.dart';
 
 class SplashPage extends StatefulWidget {
-  const SplashPage({super.key});
+  const SplashPage({
+    super.key,
+    required this.screenLoadingTime,
+  });
+
+  /// Fake screen loading time for the SplashPage screen, measured in seconds(sec).
+  final int screenLoadingTime;
 
   @override
   State<SplashPage> createState() => _SplashPageState();
 }
 
 class _SplashPageState extends State<SplashPage> {
+  List<Color> backgroundGradientColors = [
+    Colors.indigo,
+    Colors.deepPurple,
+    Colors.purple,
+    Colors.pink
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Wait 5 second until jumping to the Login/Register
+    Timer(
+      Duration(seconds: widget.screenLoadingTime),
+      () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const AuthPage(),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: backgroundGradientColors,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset(
+                "images/WeatherTuneLogo_ORIGINAL.png",
+                scale: 1,
+              ),
+              CircularProgressIndicator(
+                color: Colors.purple.shade900,
+                strokeAlign: 3,
+                strokeCap: StrokeCap.round,
+                strokeWidth: 8,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
