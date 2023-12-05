0282c3501ef745f57e7d4b870c7fc30fca1b1856
JoeyCM <99725590+JoeyCM@users.noreply.github.com>
2023-12-05 17:09:17 +0100

    radar pos set to barcelona and code cleaned

--- weather_tune/lib/pages/radar_page.dart
+++ weather_tune/lib/pages/radar_page.dart
@@ -4,58 +4,46 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:intl/intl.dart';
 
-//class RadarPage extends StatefulWidget {
-  
-//  const RadarPage({super.key});
-
-//  @override
-//  State<RadarPage> createState() => _RadarPageState();
-//}
-
-class MapSample extends StatefulWidget{
+class RadarPage extends StatefulWidget {
   @override
-  State<MapSample> createState() => MapSampleState();
+  State<RadarPage> createState() => _RadarPageState();
 }
 
-class MapSampleState extends State<MapSample> {
+class _RadarPageState extends State<RadarPage> {
   GoogleMapController? _controller;
 
   static const CameraPosition _initialPosition = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
+    target: LatLng(41.3825, 2.176944),
     zoom: 4,
   );
-  
-  DateTime _forecastDate = DateTime.now();
 
+  DateTime _forecastDate = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
-    
     Set<TileOverlay> _tileOverLays = {};
 
-    _initTiles(DateTime dateTime) async{
-
+    _initTiles(DateTime dateTime) async {
       final String overlayId = dateTime.millisecondsSinceEpoch.toString();
 
       final tileOverLay = TileOverlay(
-        tileOverlayId: TileOverlayId(overlayId),
-        tileProvider: ForecastTileProvide(
-          //mapType: 'SD0',
-          mapType: 'PR0',
-          opacity: 0.9,
-          dateTime: dateTime,
-          )
-        );
+          tileOverlayId: TileOverlayId(overlayId),
+          tileProvider: ForecastTileProvide(
+            //mapType: 'SD0',
+            mapType: 'PR0',
+            opacity: 0.9,
+            dateTime: dateTime,
+          ));
       setState(() {
         _tileOverLays = {tileOverLay};
       });
     }
 
-    _changeDate(duration){
+    _changeDate(duration) {
       setState(() {
-      _forecastDate = 
-        _forecastDate.add(duration);
+        _forecastDate = _forecastDate.add(duration);
       });
       _initTiles(_forecastDate);
     }
@@ -85,41 +73,58 @@ class MapSampleState extends State<MapSample> {
                   Positioned(
                     bottom: 30,
                     left: 30,
-                    child: ElevatedButton(style: ElevatedButton.styleFrom(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
                         shape: CircleBorder(),
                         padding: EdgeInsets.all(20),
                       ),
-                      onPressed: _controller == null ? null : (){
-                        setState(() {
-                          _changeDate(const Duration(hours: -3));
-                        });
-                        
-                      },
+                      onPressed: _controller == null
+                          ? null
+                          : () {
+                              setState(() {
+                                _changeDate(const Duration(hours: -3));
+                              });
+                            },
                       child: const Icon(
                         Icons.arrow_back_rounded,
                         color: Colors.white,
                       ),
                     ),
                   ),
-              Positioned(
-                right: 30,
-                child: ElevatedButton(style: ElevatedButton.styleFrom(
-                    shape: CircleBorder(),
-                    padding: EdgeInsets.all(20),
+                  Center(
+                    child: Card(
+                        elevation: 4,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Text(
+                              'Forecast Date: \n ${DateFormat('yyyy-MM-dd ha').format(_forecastDate)}',
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                  color: Colors.blue, fontSize: 18)),
+                        )),
                   ),
-                  onPressed: _controller == null ? null : (){
-                    setState(() {
-                      _changeDate(const Duration(hours: 3));
-                    });
-                  },
-                  child: const Icon(
-                    Icons.arrow_forward_ios_rounded,
-                    color: Colors.white,
-                  ),
-                ),
-              )
-              ],
-            ),
+                  Positioned(
+                    right: 30,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: CircleBorder(),
+                        padding: EdgeInsets.all(20),
+                      ),
+                      onPressed: _controller == null
+                          ? null
+                          : () {
+                              setState(() {
+                                _changeDate(const Duration(hours: 3));
+                              });
+                            },
+                      child: const Icon(
+                        Icons.arrow_forward_ios_rounded,
+                        color: Colors.white,
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ],
@@ -136,23 +141,24 @@ class ForecastTileProvide implements TileProvider {
 
   ForecastTileProvide({
     required this.mapType,
-    required this. opacity,
+    required this.opacity,
     required this.dateTime,
-    });
- 
+  });
+
   @override
-  Future<Tile> getTile (int x, int y, int? zoom) async {
+  Future<Tile> getTile(int x, int y, int? zoom) async {
     Uint8List tileBytes = Uint8List(0);
     final date = dateTime.millisecondsSinceEpoch ~/ 1000;
 
-    try{
-      final url = "http://maps.openweathermap.org/maps/2.0/weather/$mapType/$zoom/$x/$y?date=$date&opacity=$opacity&appid=9521fffe8c47941cbe4fd2102ef11043";
+    try {
+      final url =
+          "http://maps.openweathermap.org/maps/2.0/weather/$mapType/$zoom/$x/$y?date=$date&opacity=$opacity&appid=9521fffe8c47941cbe4fd2102ef11043";
       final uri = Uri.parse(url);
       final imageData = await NetworkAssetBundle(uri).load("");
-      tileBytes =imageData.buffer.asUint8List();
-    } catch(e){
+      tileBytes = imageData.buffer.asUint8List();
+    } catch (e) {
       print(e.toString());
     }
-      return Tile(tileSize, tileSize, tileBytes);
+    return Tile(tileSize, tileSize, tileBytes);
   }
 }
