30c21e029f586b3e416a2a41782e3f0cf7a21fdb
Adria Pons <adria.pons.mensa@estudiantat.upc.edu>
2023-12-10 18:42:02 +0100

    Removed 'const' from SizedBoxes (was giving error)

--- weather_tune/lib/pages/settings_page.dart
+++ weather_tune/lib/pages/settings_page.dart
@@ -8,77 +8,114 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: Column(children: [
-      DropDownButton(
-          widgetWidth: MediaQuery.sizeOf(context).width,
-          title: "Language",
-          child: const Align(
-              alignment: Alignment.centerLeft,
-              child: Column(
-                children: [
-                  Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text("English",
-                          style: TextStyle(color: Colors.white))),
-                  SizedBox(height: 5),
-                  Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text("Spanish",
-                          style: TextStyle(color: Colors.white))),
-                  SizedBox(height: 5),
-                  Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text("French",
-                          style: TextStyle(color: Colors.white))),
-                ],
-              ))),
-      const SizedBox(height: margins),
-      DropDownButton(
-          widgetWidth: MediaQuery.sizeOf(context).width,
-          title: "Alerts",
-          child: const Align(
-              alignment: Alignment.centerLeft,
-              child: Text("English", style: TextStyle(color: Colors.white)))),
-      const SizedBox(height: margins),
-      DropDownButton(
-          widgetWidth: MediaQuery.sizeOf(context).width,
-          title: "Units",
-          child: const Align(
-              alignment: Alignment.centerLeft,
-              child: Column(
-                children: [
-                  Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text("Celsius (Cº)",
-                          style: TextStyle(color: Colors.white))),
-                  SizedBox(height: 5),
-                  Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text("Fahrenheit (Fº)",
-                          style: TextStyle(color: Colors.white))),
-                  SizedBox(height: 5),
-                  Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text("Kelvin (K)",
-                          style: TextStyle(color: Colors.white))),
-                ],
-              ))),
-      const SizedBox(height: margins),
-      DropDownButton(
-          widgetWidth: MediaQuery.sizeOf(context).width,
-          title: "Location Services",
-          child: const Align(
-              alignment: Alignment.centerLeft,
-              child: Text("English", style: TextStyle(color: Colors.white)))),
-      const SizedBox(height: margins),
-      DropDownButton(
-          widgetWidth: MediaQuery.sizeOf(context).width,
-          title: "Weather Services",
-          child: const Align(
-              alignment: Alignment.centerLeft,
-              child: Text("English", style: TextStyle(color: Colors.white)))),
-      const SizedBox(height: margins),
-    ])));
+      body: Center(
+        child: Column(
+          children: [
+            DropDownButton(
+                widgetWidth: MediaQuery.sizeOf(context).width,
+                title: "Language",
+                child: const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Column(
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "English",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Spanish",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "French",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ))),
+            SizedBox(height: margins),
+            DropDownButton(
+              widgetWidth: MediaQuery.sizeOf(context).width,
+              title: "Alerts",
+              child: const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "English",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            SizedBox(height: margins),
+            DropDownButton(
+                widgetWidth: MediaQuery.sizeOf(context).width,
+                title: "Units",
+                child: const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Column(
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Celsius (Cº)",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Fahrenheit (Fº)",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Kelvin (K)",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ))),
+            SizedBox(height: margins),
+            DropDownButton(
+              widgetWidth: MediaQuery.sizeOf(context).width,
+              title: "Location Services",
+              child: const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "English",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            SizedBox(height: margins),
+            DropDownButton(
+              widgetWidth: MediaQuery.sizeOf(context).width,
+              title: "Weather Services",
+              child: const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "English",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            SizedBox(height: margins),
+          ],
+        ),
+      ),
+    );
   }
 }
