aa59c46ec16db1fdd68cd01b630b522257ff5021
JoeyCM <99725590+JoeyCM@users.noreply.github.com>
2024-01-08 23:16:56 +0100

    Revert "test push for api key"
    This reverts commit 64a9e522f58614d79699a5fdf25a2e4f00e4b45d.

--- weather_tune/lib/data/data.dart
+++ weather_tune/lib/data/data.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: non_constant_identifier_names
 
 String API_KEY = "9521fffe8c47941cbe4fd2102ef11043";
-String Android_Key = "AIzaSyB9ffTCWV9PC0uEu7h3PPRAU7gD243HUgc";
 
 bool API_Key_Active = true;
